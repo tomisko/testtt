857727beb48fb3eadb7dc90e5b66e2e14af57a53
unknown <tonitych@LUBN7073.abs.assecobs.pl>
2015-08-11 11:19:59 +0200

    Initial commit

---
+++ bower_components/skrypt/mojSkrypt/mojSkrypt.js
@@ -0,0 +1,6 @@
+/**
+ * Created by tonitych on 2015-08-11.
+ */
+console.log("Hello World for the first time !");
+console.log("Hello World for the second time !");
+console.log("Hello World for the last time !");
